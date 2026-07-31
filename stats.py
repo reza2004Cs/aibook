7e3c59495d9dd48bf1b02de9b41d0612ec92a45e
reza <rezakarimii2004@gmail.com>
2026-07-31 21:38:46 +0330

    adding python stats

--- stats.py
+++ stats.py
@@ -1,1 +1,4 @@
-# just a place holder
+# write a stats collector in python 
+# the stats collector should collect stats from the system
+# the stats collector should be able to collect stats
+# the stats collector should be able
